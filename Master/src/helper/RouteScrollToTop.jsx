9aa84e88d71dd33eb76aff3d9fd101097cfee47a
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-19 19:49:48 +0600

    Update RouteScrollToTop.jsx

--- Master/src/helper/RouteScrollToTop.jsx
+++ Master/src/helper/RouteScrollToTop.jsx
@@ -1,13 +1,13 @@
 "use client";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useRouter } from "next/navigation";
 
 const RouteScrollToTop = () => {
-  const pathname = useLocation();
+  const router = useRouter();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [router]);
 
   return null;
 };
