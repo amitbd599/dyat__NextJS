7ed22e6babb95985ebfdff107acf69a6d4969ba6
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-19 19:43:27 +0600

    add icon.ico

--- Master/src/app/page.jsx
+++ Master/src/app/page.jsx
@@ -1,17 +1,11 @@
-import DemoLayer from "@/demo/DemoLayer";
-
 export const metadata = {
-  title: "EduAll  - LMS, Tutors, Education & Online Course NEXT JS Template",
+  title: "DYAT - eSports and Gaming NFT NEXT JS Template",
   description:
-    "EduAll is a comprehensive and modern NEXT JS template designed for online education platforms, learning management systems (LMS), tutors, educational institutions, and online courses. It’s the perfect solution for creating an engaging and interactive online learning experience for students, educators, and institutions. Whether you’re offering online courses, running a tutoring platform, or managing an educational website, EduAll provides the tools to help you succeed. This template is tailored to meet the needs of educators, administrators, and students, providing a seamless and engaging user experience.",
+    "DYAT is a modern, dynamic React template designed specifically for eSports teams, gaming communities, and NFT marketplaces. With its sleek design and powerful features, DYAT provides the perfect platform for showcasing gaming events, team profiles, NFT collections, and digital assets in the fast-growing world of gaming and blockchain.",
 };
 
 const page = () => {
-  return (
-    <>
-      <DemoLayer />
-    </>
-  );
+  return <></>;
 };
 
 export default page;
