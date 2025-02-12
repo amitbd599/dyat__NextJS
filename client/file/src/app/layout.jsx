3369461ba41f3f3c06167c0a1fc6006f5828552d
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-02-12 12:40:44 +0600

    add update

--- client/file/src/app/layout.jsx
+++ client/file/src/app/layout.jsx
@@ -11,12 +11,6 @@ import LoadPhosphorIcons from "@/helper/LoadPhosphorIcons";
 import CustomCursor from "@/helper/CustomCursor";
 import BackToTop from "@/helper/BackToTop";
 
-export const metadata = {
-  title: "EduAll  - LMS, Tutors, Education & Online Course NEXT JS Template",
-  description:
-    "EduAll is a comprehensive and modern NEXT JS template designed for online education platforms, learning management systems (LMS), tutors, educational institutions, and online courses. It’s the perfect solution for creating an engaging and interactive online learning experience for students, educators, and institutions. Whether you’re offering online courses, running a tutoring platform, or managing an educational website, EduAll provides the tools to help you succeed. This template is tailored to meet the needs of educators, administrators, and students, providing a seamless and engaging user experience.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
